1f245013c592806d06e45c51b993bc2066cd6f54
Vica <st129650@student.spbu.ru>
2024-09-21 00:06:24 +0300

    hw1 5 tasks upload

---
+++ 2024_09_20-hw-1/Project5/Source.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main(int argc, char* argv[])
+{
+	int a = 0;
+	scanf_s("%d", &a);
+	printf("%d",""The next number for the number, a, 'is', a + 1);
+	printf("%d", 'The previous number for the number', a, 'is', a - 1);
+	return EXIT_SUCCESS;
+}
