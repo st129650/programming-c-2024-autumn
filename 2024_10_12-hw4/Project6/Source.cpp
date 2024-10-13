035a0d420e952733ed953c65479c55bd392c7a22
Vica <st129650@student.spbu.ru>
2024-10-13 11:43:44 +0300

    hhww4

--- 2024_10_12-hw4/Project6/Source.cpp
+++ 2024_10_12-hw4/Project6/Source.cpp
@@ -1,5 +1,4 @@
 #include <iostream>  //?????????
-#include <cmath>
 
 int main(int argc, char* argv[])
 {
@@ -7,29 +6,30 @@ int main(int argc, char* argv[])
     scanf_s("%d", &n);
 
     int v[100];
+    int p[100];
     for (int i = 0; i < n; ++i)
     {
         scanf_s("%d", &v[i]);
     }
-    int p[100];
+
     for (int i = 0; i < n; ++i)
     {
         scanf_s("%d", &p[i]);
     }
     int max_firm_nul = 0;  
-    double max_nul = 0;  
+    double max_nul = v[0] * (p[0] / 100.0);
 
-    for (int i = 0; i < n; ++i)
+    for (int i = 1; i < n; ++i)
     {
-        double nul = v[i] * p[i] / 100;  
+        double nul = v[i] * (p[i] / 100.0);  
         if (nul > max_nul)
         {
             max_nul = nul;
-            max_firm_nul = i + 1;  
+            max_firm_nul = i;  
         }
     }
 
-    printf("%d", max_firm_nul);
+    printf("%d", max_firm_nul + 1);
 
     return EXIT_SUCCESS;
 }
